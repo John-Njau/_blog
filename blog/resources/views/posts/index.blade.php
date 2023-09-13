1921e5b2836da4cea8a1df08c8f9b47a560966a6
John Njau <johnnjaunjoroge@gmail.com>
2023-09-13 13:03:24 +0300

    feat: convert the landing page

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -1,13 +1,8 @@
-<x-layout>
-    @include('posts._header')
-    <div id="app">
-        <!-- Vue components will be rendered here -->
-        <example-component></example-component>
-        <example-counter></example-counter>
+<x-vue-layout>
+    {{-- @include('posts._header') --}}
 
-    </div>
 
-    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    {{-- <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
         @if ($posts && count($posts) > 0)
         <x-posts-grid :posts="$posts"></x-posts-grid>
@@ -16,5 +11,5 @@
         @else
         <p class="text-center">No posts yet. Please check back later.</p>
         @endif
-    </main>
-</x-layout>
+    </main> --}}
+</x-vue-layout>
